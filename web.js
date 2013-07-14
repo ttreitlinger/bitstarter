fd7590602b8c8d2cd0b172af7cd0d9fcc8a809ea
Thoams Treitlinger <ttreitlinger@gmail.com>
2013-07-14 22:18:59 +0000

    fix sending of index file - use readFileSync with utf-8

--- web.js
+++ web.js
@@ -3,14 +3,13 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var indexFile = fs.readFileSync('index.html');
+var indexFile = fs.readFileSync('index.html', 'utf-8');
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-  response.sendfile('index.html');
+  response.send(indexFile);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
